7867bda410125ea9dc85ce8457d2b3a0eafa7cfb
EvertonHimself <evertondeoliveira@gmail.com>
2024-05-09 15:28:11 -0300

    Criada a conexão com o banco de dados.
    Criada a conexão com o banco de dados.

--- GestorDeEstudantesT6/MeuBancoDeDados.cs
+++ GestorDeEstudantesT6/MeuBancoDeDados.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace GestorDeEstudantesT6
 {
     internal class MeuBancoDeDados
     {
+        private MySqlConnection conexao =
+            new MySqlConnection("datasource=localhost;port=3306;" +
+                "username=root;password=;database=sga_estudantes_bd_t6");
+            
     }
 }
